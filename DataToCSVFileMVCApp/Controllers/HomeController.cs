435620ea7d4d7f7cf07e0520e423c346cac1acec
parvathy <parvathy@LAPTOP-7UIHE5VR>
2023-02-24 15:46:30 +0530

    Data to CSV file

--- DataToCSVFileMVCApp/Controllers/HomeController.cs
+++ DataToCSVFileMVCApp/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Data.SqlClient;
 using System.Linq;
+using System.Text;
 using System.Web;
 using System.Web.Mvc;
 
@@ -13,10 +14,8 @@ namespace DataToCSVFileMVCApp.Controllers
         string connecetionstring = "Data Source=LAPTOP-7UIHE5VR\\SQLEXPRESS;Initial Catalog=EventBookingManagement;Integrated Security=True";
         public ActionResult Index()
         {
-            //iuiiui
             return View();
         }
-
         public ActionResult About()
         {
             ViewBag.Message = "Your application description page.";
@@ -37,19 +36,45 @@ namespace DataToCSVFileMVCApp.Controllers
             SqlCommand command = new SqlCommand("EventList", connection);
             command.CommandType = System.Data.CommandType.StoredProcedure;
             SqlDataReader reader = command.ExecuteReader();
-            while(reader.Read())
+            while (reader.Read())
             {
                 Event e = new Event
                 {
                     ID = Convert.ToInt32(reader["ID"]),
                     Name = Convert.ToString(reader["Name"]),
                     Quantity = Convert.ToInt32(reader["Quantity"]),
-                    Date = Convert.ToDateTime(reader["date"]),
+                    Date = Convert.ToDateTime(reader["Date"]),
                 };
                 events.Add(e);
             }
             return View(events);
         }
-
+        public ActionResult CSV()
+        {
+            List<Event> events = new List<Event>();
+            SqlConnection connection = new SqlConnection(connecetionstring);
+            connection.Open();
+            SqlCommand command = new SqlCommand("EventList", connection);
+            command.CommandType = System.Data.CommandType.StoredProcedure;
+            SqlDataReader reader = command.ExecuteReader();
+            while (reader.Read())
+            {
+                Event e = new Event
+                {
+                    ID = Convert.ToInt32(reader["ID"]),
+                    Name = Convert.ToString(reader["Name"]),
+                    Quantity = Convert.ToInt32(reader["Quantity"]),
+                    Date = Convert.ToDateTime(reader["Date"]),
+                };
+                events.Add(e);
+            }
+            var builder = new StringBuilder();
+            builder.AppendLine("ID,Name,Quantity,Date");
+            foreach (var i in events)
+            {
+                builder.AppendLine($"{i.ID},{i.Name},{i.Quantity},{i.Date}");
+            }
+            return File(Encoding.UTF8.GetBytes(builder.ToString()), "csv", "Event.csv");
+        }
     }
 }

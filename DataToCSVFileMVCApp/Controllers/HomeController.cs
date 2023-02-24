40322075bd482285027623d7d07e18780a5065b3
parvathy <parvathy@LAPTOP-7UIHE5VR>
2023-02-24 12:50:18 +0530

    Add project files.

---
+++ DataToCSVFileMVCApp/Controllers/HomeController.cs
@@ -0,0 +1,54 @@
+﻿using DataToCSVFileMVCApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DataToCSVFileMVCApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        string connecetionstring = "Data Source=LAPTOP-7UIHE5VR\\SQLEXPRESS;Initial Catalog=EventBookingManagement;Integrated Security=True";
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+        public ActionResult EventDisplay()
+        {
+            List<Event> events = new List<Event>();
+            SqlConnection connection = new SqlConnection(connecetionstring);
+            connection.Open();
+            SqlCommand command = new SqlCommand("EventList", connection);
+            command.CommandType = System.Data.CommandType.StoredProcedure;
+            SqlDataReader reader = command.ExecuteReader();
+            while(reader.Read())
+            {
+                Event e = new Event
+                {
+                    ID = Convert.ToInt32(reader["ID"]),
+                    Name = Convert.ToString(reader["Name"]),
+                    Quantity = Convert.ToInt32(reader["Quantity"]),
+                    Date = Convert.ToDateTime(reader["date"]),
+                };
+                events.Add(e);
+            }
+            return View(events);
+        }
+
+    }
+}
---
+++ DataToCSVFileMVCApp/Models/Event.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DataToCSVFileMVCApp.Models
+{
+    public class Event
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public int Quantity { get; set; }
+        public DateTime Date { get; set; }
+    }
+}
